f444697574a009ec74b8888368f044d59adf6967
sunchhay395 <sunchhay395@gmail.com>
2023-04-21 00:11:39 +0700

    add tailwind, react to laravel project

--- FactoryProject/vite.config.js
+++ FactoryProject/vite.config.js
@@ -1,11 +1,28 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
-export default defineConfig({
+export default defineConfig(({command}) => ({
+    base: command === 'serve' ? '' : '/build/',
+    publicDir: 'fake_dir_so_nothing_gets_copied',
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: 'resources/js/app.js',
+        },
+    },
     plugins: [
+        reactRefresh(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-});
+}));
+
+
+
+
+
+
