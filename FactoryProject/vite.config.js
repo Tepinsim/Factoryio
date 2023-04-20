8e716ec6563882edb53160ce3f7908a95843e4d1
sunchhay395 <sunchhay395@gmail.com>
2023-04-21 02:02:07 +0700

    add react to laravel

--- FactoryProject/vite.config.js
+++ FactoryProject/vite.config.js
@@ -1,28 +1,11 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import reactRefresh from '@vitejs/plugin-react-refresh';
 
-export default defineConfig(({command}) => ({
-    base: command === 'serve' ? '' : '/build/',
-    publicDir: 'fake_dir_so_nothing_gets_copied',
-    build: {
-        manifest: true,
-        outDir: 'public/build',
-        rollupOptions: {
-            input: 'resources/js/app.js',
-        },
-    },
+export default defineConfig({
     plugins: [
-        reactRefresh(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-}));
-
-
-
-
-
-
+});
